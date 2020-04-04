d294c1b7a0b5fac6f711337b55513b28bb37f241
Aniol Marti <aniol@aniolmarti.cat>
2020-04-04 18:30:20 +0200

    Pujo l'activitat 3 de la 2a part de la P1

--- P2/src/life-3.hs
+++ P2/src/life-3.hs
@@ -47,7 +47,60 @@ initial = Game
     }
 
 -----------------------------------------------------
--- A completar per l'estudiant
-...
+-- Event processing
 
+pointToPos :: Point -> Game -> Pos
+pointToPos p game =
+    let (gx, gy) = (1.0 / gmZoom game) *^ p ^-^ gmShift game
+    in (round gx, round gy)
 
+handleEvent :: Event -> Game -> Game
+handleEvent (KeyDown "N") game =                -- Next generation
+    setGmBoard (nextGeneration (gmBoard game)) game
+
+handleEvent (MouseDown (x, y)) game =           -- Set live/dead cells
+    let pos = pointToPos (x, y) game
+        brd = gmBoard game
+    in setGmBoard (setCell (not $ cellIsLive pos brd) pos brd) game
+
+-- The compiler defines a function gmGridMode :: Game -> GridMode
+handleEvent (KeyDown "G") game =                -- Change grid
+    let nextGrid = case (gmGridMode game) of NoGrid    -> LivesGrid
+                                             LivesGrid -> ViewGrid
+                                             ViewGrid  -> NoGrid
+    in setGmGridMode nextGrid game
+
+handleEvent (KeyDown "I") game =                -- Zoom in
+    if gmZoom game < 2.0 then setGmZoom (gmZoom game * 2.0) game
+    else game
+
+handleEvent (KeyDown "O") game =                -- Zoom out
+    if gmZoom game > 1/16 then setGmZoom (gmZoom game / 2.0) game
+    else game
+
+handleEvent (KeyDown "ARROWUP") game =           -- Down shift
+    setGmShift (gmShift game ^-^  (1.0 / gmZoom game) *^  (0, 5)) game
+
+handleEvent (KeyDown "ARROWDOWN") game =         -- Up shift
+    setGmShift (gmShift game ^+^  (1.0 / gmZoom game) *^  (0, 5)) game
+
+handleEvent (KeyDown "ARROWRIGHT") game =        -- Left shift
+    setGmShift (gmShift game ^-^  (1.0 / gmZoom game) *^  (0, 5)) game
+
+handleEvent (KeyDown "ARROWLEFT") game =         -- Right shift
+    setGmShift (gmShift game ^+^  (1.0 / gmZoom game) *^  (0, 5)) game
+
+handleEvent _ game =                            -- Ignore other events
+    game
+
+-----------------------------------------------------
+-- Drawing
+
+zoomAndMove :: Double -> (Double, Double) -> Drawing -> Drawing
+zoomAndMove zoom (x,y) draw = scaled zoom zoom (translated x y draw)
+
+draw game =
+    zoomAndMove (gmZoom game) (gmShift game) (drawBoard (gmBoard game)) <> zoomAndMove (gmZoom game) (gmShift game) (gridDraw (gmGridMode game))
+    where gridDraw NoGrid = blank
+          gridDraw LivesGrid = drawGrid (minLiveCell (gmBoard game)) (maxLiveCell (gmBoard game))
+          gridDraw ViewGrid = drawGrid (pointToPos ((-viewWidth/2), (-viewHeight/2)) game) (pointToPos (viewWidth/2, viewHeight/2) game)
